abf8605cc171da4957753792e08f26f6eb22caa9
Daniel Stratton <d.stratton93@gmail.com>
2017-01-26 11:52:37 -0500

    Implementing List<t>
    Using a list to hold all the dashbaord names.

--- AutoUpdateDashboards/Program.cs
+++ AutoUpdateDashboards/Program.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections.Generic;
 namespace AutoUpdateDashboards
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
+            string[] dashboardNames = { "Bedford", "Blacksburg", "Botetourt", "Cave Spring", "Christiansburg", "Fairlawn", "Galax"};
+            addListValues(dashboardNames);
+        }
+
+        static List<string> addListValues(string[] x)
+        {
+            List<string> t = new List<string>();
+            foreach (string v in x)
+            {
+                t.Add(v);
+            }
+            return t;
+        }
+        static void printListValues(List<string> w)
+        {
+            foreach (string s in w)
+            {
+                Console.WriteLine(s);
+            }
+            Console.Read();
         }
     }
 }
