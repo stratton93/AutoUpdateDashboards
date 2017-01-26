ef4d569bac20fe4e20cc5f1801f364212a46c2a8
Daniel Stratton <d.stratton93@gmail.com>
2017-01-26 12:20:55 -0500

    Testing the renameFiles()
    Just want to make sure that the list values are safe until this point.

--- AutoUpdateDashboards/Program.cs
+++ AutoUpdateDashboards/Program.cs
@@ -46,9 +46,16 @@ namespace AutoUpdateDashboards
             finally
             {
                 //Going to rename the files before moving them.
+                renameFiles(x) //this is going to be doing some wizarding shit right here.
 
             }
         }
+
+        static void renameFiles(List<FileInfo> x)
+        {
+            printListValues(x);
+        }
+
         static List<FileInfo> addListValues(FileInfo[] x)
         {
             List<FileInfo> t = new List<FileInfo>();
