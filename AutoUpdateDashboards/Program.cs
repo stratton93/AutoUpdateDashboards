9528f8b764576cde97b782aef573f266d6aa6c21
Daniel Stratton <d.stratton93@gmail.com>
2017-01-27 10:09:49 -0500

    Starting over
    to hell with this...

--- AutoUpdateDashboards/Program.cs
+++ AutoUpdateDashboards/Program.cs
@@ -4,100 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using System.Collections.Generic;
 namespace AutoUpdateDashboards
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //The source stays the same no matter what. Unless dir is changed this needs to change.
-            const string source = @"C:\Users\Stratton\Downloads\";
-            string date = DateTime.Today.ToString();
-            //The dest var won't be constant because this needs to happen everday.
-            string dest = Path.Combine(@"C:\Users\Stratton\Desktop\Dashboards\", date);
-            removeInvalidChars(dest);
-            moveFiles(addListValues(getFilesFromDownloads(source)), dest);
-        }
 
-        //Needed to scrub out unwanted input. Credit of stackoverflow user Tim Schmelter.
-        static string removeInvalidChars(string d)
-        {
-            string invalidChars = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
-            foreach (char c in invalidChars)
-            {
-                d = d.Replace(c.ToString(), ""); // or with "."
-            }
-            return d;
-        }
-
-        static FileInfo[] getFilesFromDownloads(string source)
-        {
-            DirectoryInfo di = new DirectoryInfo(source);
-            FileInfo[] fi = di.GetFiles();
-            return fi;
-        }
-
-        static void moveFiles(List<FileInfo> x, string dirName) 
-        {
-            //Try to create the new dir for the new files.
-            //Exampple: 01-25-2017 -> This will hold all the jpgs.
-            try
-            {
-                //Create or re-create the new dir.
-                if (!Directory.Exists(dirName))
-                {
-                    Directory.CreateDirectory(dirName);
-                }
-                else
-                {
-                    //If the Directory already exists, then delete it.
-                    Directory.Delete(dirName);
-                }
-            }
-            catch(Exception e)
-            {
-                String s = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
-            }
-            finally
-            {
-                //Going to rename the files before moving them.
-                //renameFiles(x); //this is going to be doing some wizarding shit right here.
-
-            }
-        }
-
-        static void renameFiles(List<FileInfo> x)
-        {
-            string s;
-            int count = 1;
-            foreach(FileInfo fi in x)
-            {
-                s = fi.ToString();
-                if(s.Contains("100 (" + count.ToString() + ")" ))
-                {
-
-                }
-            }
-        }
-
-        //Need to remove this.
-        static List<FileInfo> addListValues(FileInfo[] x)
-        {
-            List<FileInfo> t = new List<FileInfo>();
-            foreach (FileInfo v in x)
-            {
-                t.Add(v);
-            }
-            return t;
-        }
-        static void printListValues(List<FileInfo> w)
+        static void printListValues(FileInfo[] w)
         {
             foreach (FileInfo s in w)
             {
                 Console.WriteLine(s);
             }
             Console.Read();
+        }
+
+        static void Main(string[] args)
+        {
+            //The source stays the same no matter what. Unless dir is changed this needs to change.
+            const string source = @"C:\Users\Stratton\Downloads\";
+            string date = DateTime.Today.ToString("MM-dd-yyyy");
+            //The dest var won't be constant because this needs to happen everday.
+            string dest = Path.Combine(@"C:\Users\Stratton\Desktop\Dashboards\", date + @"\");
         }
     }
 }
